27fe4dc6017c4b73a9bb41022aa9f1e978136ae8
vimalkuriensam <vimalkuriensam@gmail.com>
2024-05-31 21:33:19 +0100

    added userToAccountInfo storageMap

--- substrate-node-template/pallets/template/src/lib.rs
+++ substrate-node-template/pallets/template/src/lib.rs
@@ -10,7 +10,10 @@ pub use weights::*;
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use frame_support::pallet_prelude::*;
+	use frame_support::{
+		pallet_prelude::{OptionQuery, *},
+		Blake2_128Concat,
+	};
 	use frame_system::pallet_prelude::*;
 
 	#[pallet::pallet]
@@ -20,9 +23,22 @@ pub mod pallet {
 	pub trait Config: frame_system::Config {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 		type WeightInfo: WeightInfo;
+		#[pallet::constant]
+		type MaxLength: Get<u32>;
+	}
+
+	#[derive(Debug, Encode, Decode, Default, MaxEncodedLen, TypeInfo)]
+	#[scale_info(skip_type_params(T))]
+	pub struct User<T: Config> {
+		pub name: BoundedVec<u8, T::MaxLength>,
+		pub age: u8,
+		pub title: BoundedVec<u8, T::MaxLength>,
 	}
 
 	#[pallet::storage]
+	pub type AccountToUserInfo<T: Config> =
+		StorageMap<_, Blake2_128Concat, T::AccountId, User<T>, OptionQuery>;
+
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
